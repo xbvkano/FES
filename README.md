f03cd85ac6aca411576a4434ff738a07c81806bc
Marcos Kano <71046148+xbvkano@users.noreply.github.com>
2023-08-02 10:43:23 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # FES
-FES project for my internship at DRIVEN
+
+**FES machine simulator**
+
+I made This simple code for my internship at Driven NeuroRecovery Center as my intern project. I noticed that when we started the internship, we had to use the actual machine to get used to it, which took longer since we wouldn't be able to use it all the time. This simulator can help future interns get used to it better and learn some valuable keywords that will be useful during their time there! This is not by any chance original from the creators of the FES machine and has some differences to fit an online platform. Mostly the same! 
+
+Don't worry about the "help" button ;)
---
+++ script.js
@@ -0,0 +1,343 @@
+class Login{
+    constructor(ID, password){
+        this.ID = ID;
+        this.password = password;
+        this.IDLogin = "";
+        this.passwordLogin = "";
+        this.repeat = "";
+        this.ClinicianID = "1776";
+        this.ClinicianLogin = "";
+        this.LoggedIn = false;
+        this.clinicianLoggedIn = false;
+    }
+    // below are the getters and setters
+    getID(){
+        return this.ID;
+    }
+
+    getIDLogin(){
+        return this.IDLogin;
+    }
+
+    getPassword(){
+        return this.password;
+    }
+
+    getPasswordLogin(){
+        return this.passwordLogin;
+    }
+
+    // Above getters and setters
+
+    verifyID(){
+        if(this.ID == this.IDLogin){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    verifyPassword(){
+        if(this.password == this.passwordLogin){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    verifyClinician(){
+        if(this.ClinicianID == this.ClinicianLogin){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    clear(){
+        this.IDLogin = '';
+        this.passwordLogin = '';
+        this.repeat = "";
+        this.ClinicianLogin = "";
+    }
+    
+    PatientLogInOut(){
+        this.LoggedIn ? this.LoggedIn = false : this.LoggedIn = true;
+    }
+
+    ClinicianLogInOut(){
+        this.clinicianLoggedIn ? this.clinicianLoggedIn = false : this.clinicianLoggedIn = true;
+    }
+
+    IsClinicianLogIn(){
+        return this.clinicianLoggedIn;
+    }
+
+    IsPatientLogIn(){
+        return this.LoggedIn;
+    }
+
+
+
+    addID(IDNumber){
+        this.IDLogin += IDNumber;
+        return this.IDLogin;
+    }
+    addPassword(passwordNumber){
+        this.passwordLogin += passwordNumber;
+        return this.passwordLogin;
+    }
+
+    addClinician(ClinicianNumber){
+        this.ClinicianLogin += ClinicianNumber;
+        return this.ClinicianLogin;
+    }
+
+
+    updateDisplay(test){
+        if (test == "[data-PasswordDisplay]"){
+            if (this.passwordLogin.length > 0){
+                this.repeat += "•";
+            }
+            document.querySelector(test).innerHTML =  this.repeat;
+        }
+        else if (test == "[data-IdDisplay]"){
+            document.querySelector(test).innerHTML = this.IDLogin;
+        }
+        else if (test == "[data-ClinicianDisplay]"){
+            if (this.ClinicianLogin.length > 0){
+                this.repeat += "•";
+            }
+            if (Unmask.checked){
+                document.querySelector(test).innerHTML =  this.ClinicianLogin;
+            }
+            else {
+                document.querySelector(test).innerHTML =  this.repeat;
+            }
+        }
+        else if (test == "all"){
+            document.querySelector("[data-PasswordDisplay]").innerHTML = this.repeat;
+            document.querySelector("[data-IdDisplay]").innerHTML = this.IDLogin;
+            document.querySelector("[data-ClinicianDisplay]").innerHTML = this.repeat;
+        }
+    }
+};
+
+
+// Creationg of variables
+const ID = new Login('1974', '1234');
+const NumberButtons = document.querySelectorAll('[data-number]');
+
+const SubmitIDForVerify = document.querySelector('[data-SubmitID]');
+const SubmitPasswordForVerify = document.querySelector('[data-SubmitPassword]');
+const SubmitClinicianForVerify = document.querySelector('[data-SubmitClinician]');
+
+const ClearButton = document.querySelectorAll('[data-clear]');
+const PatientLogin = document.querySelector('[data-PatientLogin]');
+const ClinicianLogin = document.querySelector('[data-ClinicianLogin]');
+const Unmask = document.querySelector('[data-Unmask]');
+
+const ExitButtonsWrongID = document.querySelectorAll('[data-wrongIDExit]');
+
+const WithIdToggle = document.querySelector('[data-WithId]');
+const WithoutIdToggle = document.querySelector('[data-WithoutId]');
+document.querySelector("[data-WithIdCheck]").classList.add('active');
+let IDRequireSage = true;
+
+
+
+// Non Document Functions
+
+const PatientIsLogedIn = Patient => {
+    if (Patient === true){
+        document.querySelector("[data-BasicBackgroundQR]").classList.add('active');
+        document.querySelector("[data-MuscleSettings]").classList.add('active');
+        
+    }
+    
+
+}
+
+const PatientLogOut = Patient => {
+    if (Patient === true){
+        document.querySelector("[data-BasicBackgroundQR]").classList.remove('active');
+        document.querySelector("[data-MuscleSettings]").classList.remove('active');    
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Document Functions
+console.log('script.js');
+document.addEventListener('click', e => {
+    const isDropdownButton = e.target.matches('[data-dropdown-button]');
+    if (!isDropdownButton && e.target.closest('[data-dropdown]') != null) return;
+
+    let currentDropdown;
+    if (isDropdownButton) {
+        currentDropdown = e.target.closest('[data-dropdown]');
+        currentDropdown.classList.toggle('active');
+        ID.clear();
+        ID.updateDisplay("all");
+    }
+
+    document.querySelectorAll('[data-dropdown].active').forEach(dropdown => {
+        if (dropdown === currentDropdown) return;
+        dropdown.classList.remove('active');
+        ID.clear();
+        ID.updateDisplay("all");
+    })
+})
+
+
+
+// Below are the event listeners
+
+PatientLogin.addEventListener('click', () => {
+    document.getElementById("IdRequirement").classList.add('active');
+    document.getElementById("PatientTab").classList.remove('active');
+});
+
+ClinicianLogin.addEventListener('click', () => {
+    document.getElementById("ClinicianRequirement").classList.add('active');
+    document.getElementById("ClinicianTab").classList.remove('active');
+});
+
+WithIdToggle.addEventListener('click', () => {
+    document.querySelector("[data-WithoutIdCheck]").classList.remove('active');
+    document.querySelector("[data-WithIdCheck]").classList.add('active');
+    IDRequireSage = true;
+    console.log(IDRequireSage);
+});
+
+WithoutIdToggle.addEventListener('click', () => {
+    document.querySelector("[data-WithoutIdCheck]").classList.add('active');
+    document.querySelector("[data-WithIdCheck]").classList.remove('active');
+    IDRequireSage = false;
+    console.log(IDRequireSage);
+});
+
+
+
+
+NumberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        let PasswordOrId = document.getElementById("PasswordRequirement");
+        let ClinicianOrPatient = document.getElementById("ClinicianRequirement");
+        if (ClinicianOrPatient.classList.contains('active')){
+            ID.addClinician(button.innerText.replace(/<br\s*[\/]?>/gi, "\n").split("\n")[0]); 
+            ID.updateDisplay("[data-ClinicianDisplay]");
+        }
+        else if (PasswordOrId.classList.contains('active')){
+            ID.addPassword(button.innerText.replace(/<br\s*[\/]?>/gi, "\n").split("\n")[0]); 
+            console.log(ID.passwordLogin)
+            ID.updateDisplay("[data-PasswordDisplay]");
+        } else {
+            ID.addID(button.innerText.replace(/<br\s*[\/]?>/gi, "\n").split("\n")[0]); 
+            ID.updateDisplay("[data-IdDisplay]");
+        }
+        
+        
+    })
+});
+
+
+
+// Verifu ID and Passwords and Clinicians
+SubmitIDForVerify.addEventListener('click', (e) => {
+    if(ID.verifyID() == true){
+        console.log('ID is correct');
+        ID.clear();
+        ID.updateDisplay("[data-IdDisplay]");
+        ID.updateDisplay("[data-PasswordDisplay]");
+        let currentDropdown;
+        currentDropdown = e.target.closest('[data-dropdown]');
+        currentDropdown.classList.remove('active');
+        
+        document.getElementById("PasswordRequirement").classList.add('active');
+    }
+    else{
+        document.getElementById("WrongID").classList.add('active');
+        ID.clear();
+        ID.updateDisplay("[data-IdDisplay]");
+    }
+});
+
+SubmitPasswordForVerify.addEventListener('click', (e) => {
+    if(ID.verifyPassword() == true){
+        console.log('Password is correct');
+        ID.clear();
+        ID.updateDisplay("[data-IdDisplay]");
+        ID.updateDisplay("[data-PasswordDisplay]");
+        let currentDropdown;
+        currentDropdown = e.target.closest('[data-dropdown]');
+        currentDropdown.classList.remove('active');
+        // TODO: Add here the function for runing it based on a client
+        PatientIsLogedIn(ID.PatientLogInOut());
+        console.log("TEST")
+        console.log(ID.IsPatientLogIn());
+    }
+    else{
+        document.getElementById("WrongPIN").classList.add('active');
+        ID.clear();
+        ID.updateDisplay("[data-PasswordDisplay]");
+    }
+});
+
+SubmitClinicianForVerify.addEventListener('click', (e) => {
+    if(ID.verifyClinician() == true){
+        ID.clear();
+        ID.updateDisplay("all");
+        let currentDropdown;
+        currentDropdown = e.target.closest('[data-dropdown]');
+        currentDropdown.classList.remove('active');
+        ID.ClinicianLogInOut();
+    }
+    else{
+        document.getElementById("WrongClinicianID").classList.add('active');
+        ID.clear();
+        ID.updateDisplay("all");
+    }
+});
+
+
+ClearButton.forEach(button => {
+    button.addEventListener('click', () => {
+        ID.clear();
+        ID.updateDisplay("all");
+    });
+});
+
+Unmask.addEventListener('change', () => {
+    if (Unmask.checked){
+        document.querySelector("[data-ClinicianDisplay]").innerHTML =  ID.ClinicianLogin;
+    }
+    else {
+        document.querySelector("[data-ClinicianDisplay]").innerHTML =  ID.repeat;
+    }
+});
+
+ExitButtonsWrongID.forEach(button => {
+    button.addEventListener('click', () => {
+        document.getElementById("WrongID").classList.remove('active');
+        document.getElementById("WrongPIN").classList.remove('active');
+        document.getElementById("WrongClinicianID").classList.remove('active');
+        ID.clear();
+        ID.updateDisplay("all");
+    });
+});
+
